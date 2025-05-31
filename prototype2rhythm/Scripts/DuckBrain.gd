cd21624d7d807f45973425583aaf24f4d1cdf73d
Simon Du Plooy <47170836+SimonDuPlooyGit@users.noreply.github.com>
2025-05-31 19:31:41 +0200

    Initial planned build up section

--- prototype2rhythm/Scripts/DuckBrain.gd
+++ prototype2rhythm/Scripts/DuckBrain.gd
@@ -1,6 +1,5 @@
 extends AnimatedSprite2D
 
-@onready var spawn_spot = $SpawnSpot
 var waterbeam = preload("res://Scenes/water_beam.tscn")
 var droplet = preload("res://Scenes/droplet.tscn")
 
@@ -14,12 +13,11 @@ var sec_per_measure := sec_per_beat * 4
 var total_notes_per_measure := notes_per_beat * beats_per_measure
 
 #Current information
-var current_note = 0;
-var current_beat = 0;
-var current_measure = 0;
-var facing_right = false;
-var teleport_index = 0;
-var note_to_compare = 0; #This is for checking according to a note that I get from beatToNote or measureToNote
+var current_note = 0
+var current_beat = 0
+var current_measure = 0
+var teleport_index = 0
+var initial_scale = 0
 
 #Obstacle according to note library
 var obstacle_schedule = {
@@ -78,45 +76,47 @@ var obstacle_schedule = {
 	219: ["spawnDroplet", Vector2(1560,0), "down"],
 	220: ["spawnDroplet", Vector2(1320,0), "down"],
 	222: ["spawnDroplet", Vector2(1080,0), "down"],
-	224: ["spawnDroplet", Vector2(1320,0), "down"],
-	226: ["spawnDroplet", Vector2(1080,0), "down"],
-	227: ["spawnDroplet", Vector2(840,0), "down"],
-	228: ["spawnDroplet", Vector2(600,0), "down"],
+	224: ["spawnDroplet", Vector2(840,0), "down"],
+	226: ["spawnDroplet", Vector2(600,0), "down"],
+	227: ["spawnDroplet", Vector2(360,0), "down"],
+	228: ["spawnDroplet", Vector2(120,0), "down"],
 	230: ["spawnDroplet", Vector2(360,0), "down"],
-	232: ["spawnDroplet", Vector2(120,0), "down"],
-	233: ["spawnDroplet", Vector2(360,0), "down"],
-	234: ["spawnDroplet", Vector2(600,0), "down"],
-	235: ["spawnDroplet", Vector2(840,0), "down"],
-	236: ["spawnDroplet", Vector2(1080,0), "down"],
-	237: ["spawnDroplet", Vector2(1320,0), "down"],
+	232: ["spawnDroplet", Vector2(600,0), "down"],
+	233: ["spawnDroplet", Vector2(840,0), "down"],
+	234: ["spawnDroplet", Vector2(1080,0), "down"],
+	235: ["spawnDroplet", Vector2(1320,0), "down"],
+	236: ["spawnDroplet", Vector2(1560,0), "down"],
+	237: ["spawnDroplet", Vector2(1800,0), "down"],
 	238: ["spawnDroplet", Vector2(1560,0), "down"],
 	
 	#Beamshooting for buildup
-	beatToNote(1, 24): ["shootBeam", Vector2(1700,116), "left"],
-	beatToNote(2, 24): ["shootBeam", Vector2(1700,236), "left"],
-	beatToNote(3, 24): ["shootBeam", Vector2(1700,356), "left"],
-	beatToNote(4, 24): ["shootBeam", Vector2(1700,476), "left"],
-	beatToNote(1, 25): ["shootBeam", Vector2(1700,596), "left"],
-	beatToNote(2, 25): ["shootBeam", Vector2(1700,836), "left"],
-	beatToNote(3, 25): ["shootBeam", Vector2(1700,716), "left"],
-	beatToNote(4, 25): ["shootBeam", Vector2(1700,596), "left"],
-	beatToNote(1, 26): ["shootBeam", Vector2(1700,476), "left"],
-	beatToNote(2, 26): ["shootBeam", Vector2(1700,356), "left"],
-	beatToNote(3, 26): ["shootBeam", Vector2(1700,116), "left"],
-	beatToNote(4, 26): ["shootBeam", Vector2(1700,236), "left"],
-	beatToNote(1, 27): ["shootBeam", Vector2(1700,356), "left"],
-	beatToNote(2, 27): ["shootBeam", Vector2(1700,476), "left"],
-	beatToNote(3, 27): ["shootBeam", Vector2(1700,596), "left"],
-	beatToNote(4, 27): ["shootBeam", Vector2(1700,836), "left"],
-	beatToNote(1, 28): ["shootBeam", Vector2(1700,716), "left"],
-	beatToNote(2, 28): ["shootBeam", Vector2(1700,596), "left"],
-	beatToNote(3, 28): ["shootBeam", Vector2(1700,476), "left"],
-	beatToNote(4, 28): ["shootBeam", Vector2(1700,356), "left"],
-	beatToNote(1, 29): ["shootBeam", Vector2(1700,116), "left"],
-	beatToNote(2, 29): ["shootBeam", Vector2(1700,236), "left"],
-	beatToNote(3, 29): ["shootBeam", Vector2(1700,356), "left"],
-	beatToNote(4, 29): ["shootBeam", Vector2(1700,476), "left"],
+	beatToNote(1, 24): ["shootBeam", Vector2(1700,65), "left"],
+	beatToNote(2, 24): ["shootBeam", Vector2(1700,185), "left"],
+	beatToNote(3, 24): ["shootBeam", Vector2(1700,305), "left"],
+	beatToNote(4, 24): ["shootBeam", Vector2(1700,425), "left"],
+	beatToNote(1, 25): ["shootBeam", Vector2(1700,545), "left"],
+	beatToNote(2, 25): ["shootBeam", Vector2(1700,665), "left"],
+	beatToNote(3, 25): ["shootBeam", Vector2(1700,785), "left"],
+	beatToNote(4, 25): ["shootBeam", Vector2(1700,905), "left"],
+	beatToNote(1, 26): ["shootBeam", Vector2(1850,1080), "up"],
+	beatToNote(2, 26): ["shootBeam", Vector2(1720,1080), "up"],
+	beatToNote(3, 26): ["shootBeam", Vector2(1590,1080), "up"],
+	beatToNote(4, 26): ["shootBeam", Vector2(1460,1080), "up"],
+	beatToNote(1, 27): ["shootBeam", Vector2(1330,1080), "up"],
+	beatToNote(2, 27): ["shootBeam", Vector2(1200,1080), "up"],
+	beatToNote(3, 27): ["shootBeam", Vector2(1070,1080), "up"],
+	beatToNote(4, 27): ["shootBeam", Vector2(940,1080), "up"],
+	beatToNote(1, 28): ["shootBeam", Vector2(810,1080), "up"],
+	beatToNote(2, 28): ["shootBeam", Vector2(680,1080), "up"],
+	beatToNote(3, 28): ["shootBeam", Vector2(550,1080), "up"],
+	beatToNote(4, 28): ["shootBeam", Vector2(420,1080), "up"],
+	beatToNote(1, 29): ["shootBeam", Vector2(290,1080), "up"],
+	beatToNote(2, 29): ["shootBeam", Vector2(1700,545), "left"],
+	beatToNote(3, 29): ["shootBeam", Vector2(1700,425), "left"],
+	beatToNote(4, 29): ["shootBeam", Vector2(1700,665), "left"],
 }
+
+var duck_teleport_schedule = {}
 
 #Quacks at start of song
 var initialQuacks = [117,118,120,122,123,124,126,133,134,136,138,139,140,142,
@@ -124,23 +124,19 @@ var initialQuacks = [117,118,120,122,123,124,126,133,134,136,138,139,140,142,
 182,184,186,187,188,190,197,198,200,202,203,204,206,213,214,216,218,219,220,
 222,224,226,227,228,230,232,233,234,235,236,237,238]
 
-#Teleporting locations for the build up
-var buildupTeleportYs = [180,300,420,540,660,900,780,660,540,420,180,300,420,
-540,660,900,780,660,540,420,180,300,420,540]
-
 #Initial setup of duck
 func _ready() -> void:
+	initial_scale = self.scale
 	teleport_index = 0
 	swoopInBig()
 
 #Shoot a water beam from the duck's mouth with telegraphing
 func shootBeam(pos, dir):
 	var beam_instance = waterbeam.instantiate()
-	if (dir == "right"):
-		beam_instance.scale.x *= -1
+	beam_instance.direction = dir
 	beam_instance.global_position = pos
 	beam_instance.telegraph_time = sec_per_measure
-	beam_instance.active_time = sec_per_beat * 2
+	beam_instance.active_time = sec_per_measure
 	get_tree().current_scene.add_child(beam_instance)
 
 #Shoot a droplet from the duck's mouth with telegraphing
@@ -156,9 +152,18 @@ func spawnDroplet(pos, dir):
 	get_tree().current_scene.add_child(droplet_instance)
 
 #Flip the sprite the other way
-func flipSprite():
-	self.scale.x *= -1
-	facing_right = !facing_right
+func lookRight():
+	self.scale.x = initial_scale.x * -1
+
+func lookLeft():
+	self.rotation_degrees = 0
+	self.scale = initial_scale
+
+func lookUp():
+	self.rotation_degrees = 90
+
+func lookDown():
+	self.rotation_degrees = -90
 
 #Move straight sideways to a x pos
 func moveSideways(xCoord, duration):
@@ -178,10 +183,14 @@ func teleport(xCoord, yCoord, lookingWhere):
 
 	# Move and flip
 	position = Vector2(xCoord, yCoord)
-	if lookingWhere == "left" and facing_right:
-		flipSprite()
-	elif lookingWhere == "right" and not facing_right:
-		flipSprite()
+	if lookingWhere == "left":
+		lookLeft()
+	elif lookingWhere == "right":
+		lookRight()
+	elif lookingWhere == "up":
+		lookUp()
+	elif lookingWhere == "down":
+		lookDown()
 
 	var fade_in = create_tween()
 	fade_in.tween_property(self, "modulate:a", 1.0, 0.1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
@@ -240,12 +249,6 @@ func duckMeasureUpdate(measure):
 #Updates on the beat signal that don't need telegraphing
 func duckBeatUpdate(beat):
 	current_beat = beat;
-	
-	#Buildup shooting section
-	if (current_measure >= 24 && current_measure <= 29):
-		play("QuackSlower")
-		teleport(1700,buildupTeleportYs[teleport_index],"left")
-		teleport_index += 1
 
 #Updates on the note signal (mostly no telegraphing except the obstacle schedule)
 func duckNoteUpdate(note):
@@ -262,10 +265,19 @@ func duckNoteUpdate(note):
 		match action:
 			"shootBeam":
 				shootBeam(position, direction)
+				duck_teleport_schedule[future_note] = [position, direction]
 			"spawnDroplet":
 				spawnDroplet(position, direction)
 			"shootDroplet":
 				shootDroplet(position, direction)
+	
+	#Handle teleports
+	if duck_teleport_schedule.has(current_note):
+		var teleport_data = duck_teleport_schedule[current_note]
+		var target_pos = teleport_data[0]
+		var look_dir = teleport_data[1]
+		teleport(target_pos.x, target_pos.y, look_dir)
+		duck_teleport_schedule.erase(current_note)  # Prevent repeat
 	
 	#Move down
 	if (note == 48):
@@ -277,12 +289,12 @@ func duckNoteUpdate(note):
 	
 	#Flip left side and move up
 	if (note == 292):
-		flipSprite()
+		lookRight()
 		moveVertically(190,sec_per_measure*4)
 	
 	#Flip right side
 	if (note == 360):
-		flipSprite()
+		lookLeft()
 	
 	#Drop quack
 	if (current_note == 493):
--- prototype2rhythm/Scripts/waterbeamlogic.gd
+++ prototype2rhythm/Scripts/waterbeamlogic.gd
@@ -5,8 +5,20 @@ extends Area2D
 
 var telegraph_time := 0.0 # Time in seconds before it becomes active
 var active_time := 0.0 #Time in seconds where it shows on screen
+var direction := ""
 
 func _ready() -> void:
+	
+	match direction:
+		"left":
+			pass
+		"right":
+			pass
+		"up":
+			rotation_degrees = 90
+		"down":
+			pass
+	
 	# Initial state: invisible and harmless
 	modulate.a = 0.3
 	collision_shape.disabled = true
